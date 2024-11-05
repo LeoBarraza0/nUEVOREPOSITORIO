3e837236ffbaa6543b104d7a16993de9477a484c
Leonardo Barraza <leonardoa-barrazac@unilibre.edu.co>
2024-11-05 13:51:26 -0500

    Se ha hecho el ejercicio 5

--- src/Scripts/script.js
+++ src/Scripts/script.js
@@ -1,11 +1,13 @@
 var numgen = null;
-var arreglo = new Array(5);;
+var arreglo = new Array(5);
+var indice = 0; 
+var listanum = "";
 function Validar(event){
     event.preventDefault();
     let num = parseFloat(document.getElementById('num1').value);
-    if(num >= 90){document.getElementById('mensaje').innerText = 'Aprobado con honores.';}
+    if(num >= 90){if(num>100){document.getElementById('mensaje').innerText = 'Ingrese una nota válida.';}else{document.getElementById('mensaje').innerText = 'Aprobado con honores.';}}
     else if(70 <= num && num< 90){document.getElementById('mensaje').innerText = 'Aprobado.';}
-    else if(num < 70){document.getElementById('mensaje').innerText = 'Reprobado.';}
+    else if(num < 70){if(num<0){document.getElementById('mensaje').innerText = 'Ingrese una nota válida.';}else{document.getElementById('mensaje').innerText = 'Reprobado.';}}
 }
 
 function Determinar(event){
@@ -33,4 +35,24 @@ function Adivinar(event){
     event.preventDefault();
     let adivinado = parseFloat(document.getElementById('adivinado').value);
     if(adivinado == numgen){document.getElementById("mensaje4").innerText = "¡Felicidades, adivinaste el número!"}else{document.getElementById("mensaje4").innerText = "Lo siento, el número era: "+numgen;}
+}
+function Agregar(event) {
+    event.preventDefault();
+    var numero = parseInt(document.getElementById('num51').value);
+    
+    if (indice < arreglo.length) {
+        arreglo[indice] = numero;
+        indice++;
+    } else {
+        document.getElementById('mensajealerta').innerText = '¡El arreglo está lleno!';
+    }
+}
+function Mostrar(event) {
+    event.preventDefault();
+    let listanum = "";
+
+    for (let i = 0; i < arreglo.length; i++) {
+        listanum += arreglo[i] + " ";
+    }
+    document.getElementById('mensaje5').innerText = listanum.trim();
 }
